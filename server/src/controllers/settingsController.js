d5e6037f0a06e0bdbd966df54e89f953ff8c3745
zainab271 <zainab.fatima.62403@gmail.com>
2026-08-10 14:19:40 +0500

    Fix password update issue

--- server/src/controllers/settingsController.js
+++ server/src/controllers/settingsController.js
@@ -76,18 +76,16 @@ export const updateProfile = async (req, res) => {
 export const changePassword = async (req, res) => {
   try {
     const { currentPassword, newPassword } = req.body;
-    const user = await User.findById(req.user._id);
+    const user = await User.findById(req.user._id).select("+password");
 
     if (!user) return res.status(404).json({ message: "User not found" });
 
     const isMatch = await bcrypt.compare(currentPassword, user.password);
     if (!isMatch)
       return res.status(400).json({ message: "Current password is incorrect" });
-
-    const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(newPassword, salt);
-    await user.save();
-
+    
+     user.password = newPassword;
+     await user.save();
     res.json({ message: "Password updated successfully" });
   } catch (err) {
     res.status(500).json({ message: err.message });
